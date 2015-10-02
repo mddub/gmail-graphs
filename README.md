20e1c69ae768849410551b96dd2ec7a0a3ced864
Mark Wilson <mark@warkmilson.com>
2015-10-01 21:45:21 -0700

    Add figure 4: length of time a message spends in inbox

--- README.md
+++ README.md
@@ -16,6 +16,12 @@ Follow the [Inbox count over time] IPython Notebook.
 
 See `compute_ages_on_each_day.py` and the [Inbox count by age] IPython Notebook to graph the results.
 
+### Figure 4: Length of time a message spends in inbox, by date of arrival
+
+![Figure 4: Length of time a message spends in inbox, by date of arrival](https://raw.githubusercontent.com/mddub/gmail-graphs/master/sample-output/figure-4.png)
+
+See `compute_time_in_inbox_by_date_received.py` and the [Length of time a message spends in inbox] IPython Notebook to graph the results.
+
 ### Other graphs
 
 Coming soon.
@@ -24,3 +30,4 @@ Coming soon.
 [Three Years of Logging My Inbox Count]: http://warkmilson.com/2015/05/15/three-years-of-logging-my-inbox-count.html
 [Inbox count by age]: http://nbviewer.ipython.org/github/mddub/gmail-graphs/blob/master/Inbox%20count%20by%20age.ipynb
 [Inbox count over time]: http://nbviewer.ipython.org/github/mddub/gmail-graphs/blob/master/Inbox%20count%20over%20time.ipynb
+[Length of time a message spends in inbox]: http://nbviewer.ipython.org/github/mddub/gmail-graphs/blob/master/Length%20of%20time%20a%20message%20spends%20in%20inbox.ipynb
---
+++ compute_time_in_inbox_by_date_received.py
@@ -0,0 +1,78 @@
+import os
+import sys
+from collections import defaultdict
+from datetime import datetime
+from datetime import timedelta
+from dateutil.parser import parser
+from pytz import timezone
+
+import simplejson as json
+
+try:
+    # This directory should contain files like '2015-06-01_01_00_01.json'
+    LOG_DIR = sys.argv[1]
+except IndexError:
+    print "Usage: python {0} log_directory".format(sys.argv[0])
+    sys.exit(1)
+
+log_files = [f for f in os.listdir(LOG_DIR) if f.endswith('.json')]
+timestamps_and_files = [
+    (
+        datetime.strptime(f, '%Y-%m-%d_%H.%M.%S.json'),
+        os.path.join('messages', f)
+    )
+    for f in log_files
+]
+
+timestamps_and_thread_ids = [
+    (
+        ts,
+        set([m['thread_id'] for m in json.loads(open(f).read())])
+    )
+    for ts, f in timestamps_and_files
+]
+
+all_appearances = []
+inbox_thread_to_date_of_appearance = {}
+
+for (last_date, last_threads), (date, threads) in zip(timestamps_and_thread_ids[:-1], timestamps_and_thread_ids[1:]):
+    for just_disappeared_thread in set(inbox_thread_to_date_of_appearance.keys()) - threads:
+        # The thread disappeared from the inbox at some point between last_date
+        # and date, so treat the average of last_date and date as the end of
+        # this thread's tenure in the inbox. The higher the sample frequency,
+        # the more accurate this method is.
+        date_of_disappearance = last_date + (date - last_date) / 2
+        all_appearances.append(
+            (
+                just_disappeared_thread,
+                inbox_thread_to_date_of_appearance[just_disappeared_thread],
+                date_of_disappearance - inbox_thread_to_date_of_appearance[just_disappeared_thread]
+            )
+        )
+        del inbox_thread_to_date_of_appearance[just_disappeared_thread]
+
+    for new_thread in threads - set(inbox_thread_to_date_of_appearance.keys()):
+        # When an inbox sample contains a thread which was not seen in the
+        # previous sample, consider that to be the start of this particular
+        # period of the thread being in the inbox.
+        #
+        # It is possible for one thread to have multiple such periods. A thread
+        # which appears in the inbox, is archived a day later, then reappears in
+        # the inbox because of a response, then is archived again two days
+        # later, will generate two entries, of two different lengths and on two
+        # different dates.
+        inbox_thread_to_date_of_appearance[new_thread] = date
+
+tenures_by_date_received = defaultdict(list)
+for thread_id, appearance_date, tenure_length in all_appearances:
+    # Convert dates to strings and timedeltas to hours
+    tenures_by_date_received[appearance_date.date().strftime("%Y-%m-%d")].append(
+        round(tenure_length.total_seconds() / 3600.0, 2)
+    )
+
+sorted_tenures_by_date_received = dict(
+    (key, sorted(values))
+    for key, values in tenures_by_date_received.iteritems()
+)
+
+print json.dumps(sorted_tenures_by_date_received, sort_keys=True, indent='    ')
